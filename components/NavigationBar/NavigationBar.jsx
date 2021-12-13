5ceda033bc5c0b01920e84ef0fd653e629eddd70
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2021-12-13 20:28:18 +0530

    Added Content To NavigationBar

--- components/NavigationBar/NavigationBar.jsx
+++ components/NavigationBar/NavigationBar.jsx
@@ -1,11 +1,69 @@
 import React from 'react';
+import {
+  Box,
+  Flex,
+  Button,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  IconButton,
+  Text,
+  HStack,
+} from "@chakra-ui/react";
+import {} from "@chakra-ui/icons";
+import { MoonIcon, SunIcon, HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import NavLink from './NavLink/NavLink';
 
-const NavigationBar = () =>{
-    return(
-        <div>
-            NavBar
-        </div>
-    )
-}
+
+const NavigationBar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const Links = ["Projects","About","Contact"];
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+            <IconButton
+              size={"md"}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={"Open Menu"}
+              display={{ md: "none" }}
+              onClick={isOpen ? onClose : onOpen}
+            />
+            <HStack spacing={8} alignItems={"center"}>
+              <Text>LOGO</Text>
+            </HStack>
+
+          <Flex alignItems={"center"}>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </HStack>
+          </Flex>
+        </Flex>
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+};
 
 export default NavigationBar;
+
