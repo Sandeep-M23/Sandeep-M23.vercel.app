f6f333a25d89113eaf3ae8f60a17c7831078ed7b
Sandeep M <sandeep.m24.rathnam@gmail.com>
2022-01-16 22:10:02 +0530

    Update Card.jsx

--- components/UI/Card.jsx
+++ components/UI/Card.jsx
@@ -15,15 +15,10 @@ import {
 const Card = () => {
     return (
       <Center py={6}>
-        <Stack
-          borderWidth="1px"
-          borderRadius="lg"
-          w={{ sm: '100%', md: '540px' }}
-          height={{ sm: '476px', md: '20rem' }}
-          direction={{ base: 'column', md: 'row' }}
-          bg={useColorModeValue('white', 'gray.900')}
-          boxShadow={'2xl'}
-          padding={4}>
+        <Stack>
+        <heading>
+        CARD COMPONENT
+        </heading>
         </Stack>
       </Center>
     );
