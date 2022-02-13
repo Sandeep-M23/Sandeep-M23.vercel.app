514dd4628298e919fa7a250da3b6fb417739de85
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-02-14 00:58:01 +0530

    Added Custom Steps Config

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,30 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { background, ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { StepsStyleConfig } from 'chakra-ui-steps';
 import NavigationBar from "../components/NavigationBar/NavigationBar";
+
+const CustomSteps = {
+  ...StepsStyleConfig,
+  baseStyle: props => {
+    return {
+      ...StepsStyleConfig.baseStyle(props),
+      stepIconContainer:{
+        ...StepsStyleConfig.baseStyle(props).stepIconContainer,
+        borderColor:'red',
+      },
+    };
+  },
+};
+
+
+const theme = extendTheme({
+  components: {
+    Steps:CustomSteps
+  },
+});
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <NavigationBar /> {/*CHANGED LOCATION FOR GLOBAL REFERENCE*/}
       <Component {...pageProps} />
     </ChakraProvider>
