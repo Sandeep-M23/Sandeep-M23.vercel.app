680561667c92d68f20ebe610b54a7989a9a5a910
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-02-17 03:12:29 +0530

    Removed Themes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,30 +1,9 @@
-import { background, ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { StepsStyleConfig } from 'chakra-ui-steps';
+import { background, ChakraProvider, extendTheme } from "@chakra-ui/react";
 import NavigationBar from "../components/NavigationBar/NavigationBar";
-
-const CustomSteps = {
-  ...StepsStyleConfig,
-  baseStyle: props => {
-    return {
-      ...StepsStyleConfig.baseStyle(props),
-      stepIconContainer:{
-        ...StepsStyleConfig.baseStyle(props).stepIconContainer,
-        borderColor:'red',
-      },
-    };
-  },
-};
-
-
-const theme = extendTheme({
-  components: {
-    Steps:CustomSteps
-  },
-});
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <NavigationBar /> {/*CHANGED LOCATION FOR GLOBAL REFERENCE*/}
       <Component {...pageProps} />
     </ChakraProvider>
