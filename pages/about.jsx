d64da80c1c8568d16dd5eab0d66d9e9fc1dc05da
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-02-18 22:52:45 +0530

    Added Stepper & data

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,25 +7,44 @@ import {
   ListItem,
   ListIcon,
   HStack,
-  Flex,
-  Button,
-  Contents,
   Heading,
   Divider,
+  Center,
+  Icon
 } from "@chakra-ui/react";
 import {
 MdCheckCircle
 } from "react-icons/md";
-import { Step, Steps, useSteps } from "chakra-ui-steps"
+import {BsCircleFill} from 'react-icons/bs'
 import React from 'react';
+import Stepper from '../components/UI/Stepper';
 
-
+const data = [
+  {
+    course:'Computer Science and Engineering',
+    year:'2019-2023',
+    institution:'JSS Academy of Technical Education - Bengaluru',
+    grade:'CGPA - 8.4',
+  },
+  {
+    course:'PCMB',
+    year:'2017-2019',
+    institution:'The National PU College',
+    grade:'Percentage - 78',
+  },
+  {
+    course:'ISCE',
+    year:'2006-2017',
+    institution:'Sree Rama Vidyalaya',
+    grade:'Percentage - 83.25',
+  },
+]
 
 const About = () => {
   return (
     <Container maxW={"6xl"} mt={8}>
-      <HStack spacing={20}>
-        <VStack spacing={12} align={"flex-start"} width={"58%"}>
+      <HStack spacing={28} alignItems={'flex-start'}>
+        <VStack spacing={12} align={"flex-start"} width={"53%"}>
           <Box>
             <Box mb={4}>
               <Heading mb={2}>ABOUT ME</Heading>
@@ -85,48 +104,16 @@ const About = () => {
             </HStack>
           </Box>
         </VStack>
-        <VStack spacing={5} align={"flex-start"}>
-          <Box>
-            <Steps orientation="vertical" colorScheme={"red"} size={"sm"} activeStep={-1}>
-              <Step icon={"none"} label={"Bachelor of Engineering"} >
-                <Box
-                  display={"flex"}
-                  flexDir={"column"}
-                  alignItems={"flex-start"}
-                >
-                  <Text>2019-2023</Text>
-                  <Text>Computer Science and Engineering</Text>
-                  <Text>JSS Academy of Technical Education - Bengaluru</Text>
-                  <Text>CGPA - 8.4</Text>
-                </Box>
-              </Step>
-              <Step icon={"none"} label={"Pre-University"}>
-                <Box
-                  display={"flex"}
-                  flexDir={"column"}
-                  alignItems={"flex-start"}
-                >
-                  <Text>2017-2019</Text>
-                  <Text>Science(PCMB)</Text>
-                  <Text>The National PU College</Text>
-                  <Text>Percentage - 78%</Text>
-                </Box>
-              </Step>
-              <Step icon={"none"}>
-                <Box
-                  display={"flex"}
-                  flexDir={"column"}
-                  alignItems={"flex-start"}
-                >
-                  <Text>2006-2017</Text>
-                  <Text>ICSE</Text>
-                  <Text>Sree Rama Vidyalaya</Text>
-                  <Text>Percentage - 83.25%</Text>
-                </Box>
-              </Step>
-            </Steps>
+        <Box>
+          <Box mb={4}>
+            <Heading mb={2}>EDUCATION</Heading>
+            <Divider height={"3px"} bg={"red"} />
           </Box>
-        </VStack>
+          {data.map((data, index) => (
+            <Stepper key={index} data={data} />
+          ))}
+          <Icon width={5} height={5} mx={2} as={BsCircleFill} color={'red'}/>
+        </Box>
       </HStack>
     </Container>
   );
