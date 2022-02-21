ec339a2828644c4c372a30f75650e60886894f85
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-02-22 01:45:54 +0530

    Elsint / Prettier

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,94 +22,90 @@ const Home = () => {
   const LinkBgLightModeColor = "#f4f1ee";
   return (
     <Container maxW={"6xl"} mt={24}>
-        <VStack spacing={4} align={"flex-start"}>
-          <Box p={2}>
-            <Heading
-              size={"3xl"}
-            >
-              I&apos;m Sandeep M
-            </Heading>
-          </Box>
-          <Box p={2}>
-            <Text>
-              A Web Developer! Currently pursuing 3rd year of Bachelor of
-              Engineering in Computer Science from JSS Academy of Technical
-              Education Bengaluru. I&apos;m a passionate learner who&apos;s
-              always willing to learn and work across new technologies.
-            </Text>
-          </Box>
-          <Box p={2}>
-            View my{" "}
-            <Link
-              bg={useColorModeValue(LinkBgLightModeColor, LinkBgDarkModeColor)}
-              px={3}
-              py={2}
-              rounded={6}
-              _hover={{
-                textDecoration: "none",
-                bg: "red",
-              }}
-            >
-              Projects
-            </Link>
-            ,{"  "}
-            <Link
-              bg={useColorModeValue(LinkBgLightModeColor, LinkBgDarkModeColor)}
-              px={3}
-              py={2}
-              rounded={6}
-              _hover={{
-                textDecoration: "none",
-                bg: "red",
-              }}
-            >
-              Resume
-            </Link>
-            ,{"  "}
-            <Link
-              bg={useColorModeValue(LinkBgLightModeColor, LinkBgDarkModeColor)}
-              px={3}
-              py={2}
-              rounded={6}
-              _hover={{
-                textDecoration: "none",
-                bg: "red",
-              }}
-            >
-              Contact Me
-            </Link>
-            {"  "}
-            or send me an email at{"  "}
-            <Link
-              bg={useColorModeValue(LinkBgLightModeColor, LinkBgDarkModeColor)}
-              px={3}
-              py={2}
-              rounded={6}
-              _hover={{
-                textDecoration: "none",
-                bg: "red",
-              }}
-            >
-              sandeep.m24.rathnam@gmail.com
-            </Link>
-          </Box>
-          <Box p={2}>
-            <Link _hover={{ textDecoration: "none", color: "red" }}>
-              <Flex alignItems={"center"}>
-                <Text>See More About Me</Text>
-                <Icon as={BsFillArrowRightSquareFill} w={8} h={4} m={2} />
-              </Flex>
-            </Link>
-          </Box>
-          <Box p={2} display={"flex"} flexDir={"row"} alignItems={"center"}>
-            <Link _hover={{ color: "red" }}>
-              <Icon as={BsGithub} w={10} h={6} mr={2} />
-            </Link>
-            <Link _hover={{ color: "red" }}>
-              <Icon as={BsLinkedin} w={10} h={6} />
-            </Link>
-          </Box>
-        </VStack>
+      <VStack spacing={4} align={"flex-start"}>
+        <Box p={2}>
+          <Heading size={"3xl"}>I&apos;m Sandeep M</Heading>
+        </Box>
+        <Box p={2}>
+          <Text>
+            A Web Developer! Currently pursuing 3rd year of Bachelor of
+            Engineering in Computer Science from JSS Academy of Technical
+            Education Bengaluru. I&apos;m a passionate learner who&apos;s always
+            willing to learn and work across new technologies.
+          </Text>
+        </Box>
+        <Box p={2}>
+          View my{" "}
+          <Link
+            bg={useColorModeValue(LinkBgLightModeColor, LinkBgDarkModeColor)}
+            px={3}
+            py={2}
+            rounded={6}
+            _hover={{
+              textDecoration: "none",
+              bg: "red",
+            }}
+          >
+            Projects
+          </Link>
+          ,{"  "}
+          <Link
+            bg={useColorModeValue(LinkBgLightModeColor, LinkBgDarkModeColor)}
+            px={3}
+            py={2}
+            rounded={6}
+            _hover={{
+              textDecoration: "none",
+              bg: "red",
+            }}
+          >
+            Resume
+          </Link>
+          ,{"  "}
+          <Link
+            bg={useColorModeValue(LinkBgLightModeColor, LinkBgDarkModeColor)}
+            px={3}
+            py={2}
+            rounded={6}
+            _hover={{
+              textDecoration: "none",
+              bg: "red",
+            }}
+          >
+            Contact Me
+          </Link>
+          {"  "}
+          or send me an email at{"  "}
+          <Link
+            bg={useColorModeValue(LinkBgLightModeColor, LinkBgDarkModeColor)}
+            px={3}
+            py={2}
+            rounded={6}
+            _hover={{
+              textDecoration: "none",
+              bg: "red",
+            }}
+          >
+            sandeep.m24.rathnam@gmail.com
+          </Link>
+        </Box>
+        <Box p={2}>
+          <Link _hover={{ textDecoration: "none", color: "red" }}>
+            <Flex alignItems={"center"}>
+              <Text>See More About Me</Text>
+              <Icon as={BsFillArrowRightSquareFill} w={8} h={4} m={2} />
+            </Flex>
+          </Link>
+        </Box>
+        <Box p={2} display={"flex"} flexDir={"row"} alignItems={"center"}>
+          <Link _hover={{ color: "red" }}>
+            <Icon as={BsGithub} w={10} h={6} mr={2} />
+          </Link>
+          <Link _hover={{ color: "red" }}>
+            <Icon as={BsLinkedin} w={10} h={6} />
+          </Link>
+        </Box>
+      </VStack>
     </Container>
   );
 };
