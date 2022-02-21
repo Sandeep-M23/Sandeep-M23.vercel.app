dac2312db1a7f8026751b6c5154247565ab8f75e
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-02-21 23:38:43 +0530

    Eslint / Prettier

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Projects = () => {
-  return(
+  return (
     <div>
       <h1>PROJECTS PAGE</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Projects;
