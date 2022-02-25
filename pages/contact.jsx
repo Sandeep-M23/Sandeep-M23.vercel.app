dd4cbe499d70e7dd39097202d7382e294bf448d5
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-02-25 23:09:38 +0530

    Redesigned Page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,49 +1,38 @@
 import {
   Box,
   Button,
-  Flex,
   FormControl,
   FormLabel,
-  Heading,
-  IconButton,
   Input,
   InputGroup,
   InputLeftElement,
-  Link,
   Stack,
   Textarea,
-  Tooltip,
   Container,
-  useColorModeValue,
   VStack,
-  useClipboard
-} from '@chakra-ui/react';
-import React from 'react';
-import { BsGithub, BsLinkedin, BsPerson, BsTwitter } from 'react-icons/bs';
-import { MdEmail, MdOutlineEmail } from 'react-icons/md';
+} from "@chakra-ui/react";
+import React from "react";
+import { BsPerson } from "react-icons/bs";
+import { MdOutlineEmail } from "react-icons/md";
+import ImageCard from "../components/UI/ImageCard";
 
 const Contact = () => {
-  const { hasCopied, onCopy } = useClipboard('example@example.com');
   return (
     <Container maxW={"6xl"} centerContent overflow="hidden">
-      <Box
-        borderRadius="lg"
-        m={{ base: 5, md: 16, lg: 6 }}
-        p={{ base: 5 }}
-        width={"100%"}
-      >
+      <Box borderRadius="lg" m={{ base: 5 }} p={{ base: 5 }} width={"100%"}>
         <Box width={"100%"}>
           <Stack
-            spacing={{ base: 4, md: 8, lg: 20 }}
+            spacing={{ base: 4, md: 8, lg: 28 }}
             direction={{ base: "column", md: "row" }}
+            align={"center"}
           >
+            <ImageCard />
             <Box
               borderRadius="lg"
               p={8}
               bg="white"
               color="#0B0E3F"
-              shadow="base"
-              width={"60%"}
+              width={"70%"}
             >
               <VStack spacing={5}>
                 <FormControl id="name">
@@ -100,75 +89,11 @@ const Contact = () => {
                 </FormControl>
               </VStack>
             </Box>
-            <Stack
-              align="center"
-              justify="space-around"
-              direction={{ base: "row", md: "column" }}
-            >
-              <Tooltip
-                label={hasCopied ? "Email Copied!" : "Copy Email"}
-                closeOnClick={false}
-                hasArrow
-              >
-                <IconButton
-                  aria-label="email"
-                  variant="ghost"
-                  size="lg"
-                  fontSize="3xl"
-                  icon={<MdEmail />}
-                  _hover={{
-                    color: "red.500",
-                  }}
-                  onClick={onCopy}
-                  isRound
-                />
-              </Tooltip>
-
-              <Link href="#">
-                <IconButton
-                  aria-label="github"
-                  variant="ghost"
-                  size="lg"
-                  fontSize="3xl"
-                  icon={<BsGithub />}
-                  _hover={{
-                    color: "red.500",
-                  }}
-                  isRound
-                />
-              </Link>
-
-              <Link href="#">
-                <IconButton
-                  aria-label="twitter"
-                  variant="ghost"
-                  size="lg"
-                  icon={<BsTwitter size="28px" />}
-                  _hover={{
-                    color: "red.500",
-                  }}
-                  isRound
-                />
-              </Link>
-
-              <Link href="#">
-                <IconButton
-                  aria-label="linkedin"
-                  variant="ghost"
-                  size="lg"
-                  icon={<BsLinkedin size="28px" />}
-                  _hover={{
-                    color: "red.500",
-                  }}
-                  isRound
-                />
-              </Link>
-            </Stack>
           </Stack>
         </Box>
       </Box>
     </Container>
   );
-}
+};
 
 export default Contact;
