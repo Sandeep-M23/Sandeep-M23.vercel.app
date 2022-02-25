915935e73b6c72e30d88ccd29247544fe0f3c622
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-02-25 23:12:32 +0530

    Eslint/Prettier

--- pages/about.jsx
+++ pages/about.jsx
@@ -9,41 +9,38 @@ import {
   HStack,
   Heading,
   Divider,
-  Center,
-  Icon
+  Icon,
 } from "@chakra-ui/react";
-import {
-MdCheckCircle
-} from "react-icons/md";
-import {BsCircleFill} from 'react-icons/bs'
-import React from 'react';
-import Stepper from '../components/UI/Stepper';
+import { MdCheckCircle } from "react-icons/md";
+import { BsCircleFill } from "react-icons/bs";
+import React from "react";
+import Stepper from "../components/UI/Stepper";
 
 const data = [
   {
-    course:'Computer Science and Engineering',
-    year:'2019-2023',
-    institution:'JSS Academy of Technical Education - Bengaluru',
-    grade:'CGPA - 8.4',
+    course: "Computer Science and Engineering",
+    year: "2019-2023",
+    institution: "JSS Academy of Technical Education - Bengaluru",
+    grade: "CGPA - 8.4",
   },
   {
-    course:'PCMB',
-    year:'2017-2019',
-    institution:'The National PU College',
-    grade:'Percentage - 78',
+    course: "PCMB",
+    year: "2017-2019",
+    institution: "The National PU College",
+    grade: "Percentage - 78",
   },
   {
-    course:'ISCE',
-    year:'2006-2017',
-    institution:'Sree Rama Vidyalaya',
-    grade:'Percentage - 83.25',
+    course: "ISCE",
+    year: "2006-2017",
+    institution: "Sree Rama Vidyalaya",
+    grade: "Percentage - 83.25",
   },
-]
+];
 
 const About = () => {
   return (
     <Container maxW={"6xl"} mt={8}>
-      <HStack spacing={28} alignItems={'flex-start'}>
+      <HStack spacing={28} alignItems={"flex-start"}>
         <VStack spacing={12} align={"flex-start"} width={"53%"}>
           <Box>
             <Box mb={4}>
@@ -112,11 +109,11 @@ const About = () => {
           {data.map((data, index) => (
             <Stepper key={index} data={data} />
           ))}
-          <Icon width={5} height={5} mx={2} as={BsCircleFill} color={'red'}/>
+          <Icon width={5} height={5} mx={2} as={BsCircleFill} color={"red"} />
         </Box>
       </HStack>
     </Container>
   );
-}
+};
 
 export default About;
