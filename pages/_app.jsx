01350183ed9deb9665a29c8110118a5f05278e0d
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-02-26 20:52:57 +0530

    Added Theme

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,20 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import NavigationBar from "../components/NavigationBar/NavigationBar";
+import Head from "next/head";
+import theme from "../styles/theme";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
-      <NavigationBar /> {/*CHANGED LOCATION FOR GLOBAL REFERENCE*/}
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Sandeep M</title>
+        <meta name="description" content="PortFolio Website of Sandeep M" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <NavigationBar /> {/*CHANGED LOCATION FOR GLOBAL REFERENCE*/}
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 };
 
