ed7ee7208ffb89acce2a3b4ac6b6be6c04d86ed1
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-03-03 15:10:18 +0530

    Added Contents

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import {
+  Container,
+  HStack,
+  Heading,
+  Flex,
+  Box,
+  Divider,
+} from "@chakra-ui/react";
+import ProjectCard from "../components/UI/Card/ProjectCard";
+import FeaturedCard from "../components/UI/Card/FeaturedCard";
 
 const Projects = () => {
   return (
-    <div>
-      <h1>PROJECTS PAGE</h1>
-    </div>
+    <Container maxW={"6xl"} mt={8}>
+      <Flex>
+        <FeaturedCard/>
+      </Flex>
+      <Flex flexDir={"column"} align={"center"}>
+        <Box mb={4} width={'100%'}>
+          <Heading mb={2} fontSize={32} textAlign={'center'}>OTHER NOTEWORTHY PROJECTS</Heading>
+          <Divider height={"3px"} bg={"red"}/>
+        </Box>
+        <HStack spacing={4}>
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </HStack>
+      </Flex>
+    </Container>
   );
 };
 
