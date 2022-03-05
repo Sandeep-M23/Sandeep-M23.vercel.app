455e384e0ad6b724adf99be64583d6fa114288b3
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-03-05 22:16:31 +0530

    Redesigned FeaturedCard

--- components/UI/Card/FeaturedCard.jsx
+++ components/UI/Card/FeaturedCard.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {
-  Badge,
-  Button,
   Center,
   Flex,
   Heading,
@@ -9,54 +7,64 @@ import {
   Link,
   Stack,
   Text,
-  useColorModeValue,
   HStack,
   Icon,
 } from '@chakra-ui/react';
 import {FiExternalLink} from 'react-icons/fi';
 import {BsGithub} from 'react-icons/bs';
 
-const FeaturedCard = () => {
+const FeaturedCard = ({cardDir,cardPos,cardAlign,imagePos}) => {
   return (
     <Center py={6}>
       <Stack
-        borderWidth="1px"
         borderRadius="lg"
         w={{ base: "100%" }}
-        height={{ sm: "476px", md: "30rem" }}
-        direction={{ base: "column", md: "row" }}
-        bg={useColorModeValue("white", "gray.900")}
-        boxShadow={"2xl"}
+        height={{ sm: "476px", md: "30rem", lg:'27rem' }}
+        direction={{ base: "column", md: cardDir }}
         padding={4}
       >
-        <Flex flex={1} bg="blue.200">
+        <Flex
+          flex={1}
+          bg="#2727277d"
+          w={"50%"}
+          h={"85%"}
+          position={"relative"}
+          right={imagePos}
+          overflow={"hidden"}
+        >
           <Image
-            objectFit="cover"
-            boxSize="100%"
+            boxSize={"100%"}
+            opacity={"0.4"}
+            rounded={8}
+            _hover={{
+              opacity: 1,
+            }}
             src={
               "https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
             }
           />
         </Flex>
-        <Stack
-          flex={1}
+        <Flex
+          flex={0.9}
           flexDirection="column"
-          justifyContent="center"
-          alignItems="flex-end"
+          justifyContent="flex-start"
+          alignItems={cardAlign}
           p={1}
+          position={"relative"}
+          right={cardPos}
           pt={2}
         >
-          <Text>Featured Project</Text>
+          <Text color="red.600">Featured Project</Text>
           <Heading>Project Name</Heading>
-          <Text>
+          <Text bg={"red"} p={4} rounded={8} mt={6}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua
           </Text>
-          <HStack>
+          <HStack spacing={6} my={3} color={'#c1c1c1'} fontWeight={500}>
             <Text>HTML5</Text>
             <Text>CSS3</Text>
           </HStack>
-          <HStack spacing={4}>
+          <HStack spacing={6} my={2}>
             <Link href="#">
               <Icon as={BsGithub} w={6} h={6} />
             </Link>
@@ -64,7 +72,7 @@ const FeaturedCard = () => {
               <Icon as={FiExternalLink} w={6} h={6} />
             </Link>
           </HStack>
-        </Stack>
+        </Flex>
       </Stack>
     </Center>
   );
