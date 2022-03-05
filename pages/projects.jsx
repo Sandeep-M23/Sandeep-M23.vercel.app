ba3adc1da4b7bd986487c32ee4ea07cf0a3bb266
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-03-05 22:16:46 +0530

    Added More Cards

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -6,6 +6,7 @@ import {
   Flex,
   Box,
   Divider,
+  Grid,
 } from "@chakra-ui/react";
 import ProjectCard from "../components/UI/Card/ProjectCard";
 import FeaturedCard from "../components/UI/Card/FeaturedCard";
@@ -13,19 +14,24 @@ import FeaturedCard from "../components/UI/Card/FeaturedCard";
 const Projects = () => {
   return (
     <Container maxW={"6xl"} mt={8}>
-      <Flex>
-        <FeaturedCard/>
+      <Flex flexDir={'column'} alignItems={'center'}>
+        <FeaturedCard cardDir={'row'}  cardPos={'5%'} cardAlign={'flex-end'}/>
+        <FeaturedCard cardDir={'row-reverse'} cardPos={0} imagePos={'5%'} cardAlign={'flex-start'}/>
+        <FeaturedCard cardDir={'row'} cardPos={'5%'} cardAlign={'flex-end'}/>
       </Flex>
       <Flex flexDir={"column"} align={"center"}>
         <Box mb={4} width={'100%'}>
           <Heading mb={2} fontSize={32} textAlign={'center'}>OTHER NOTEWORTHY PROJECTS</Heading>
           <Divider height={"3px"} bg={"red"}/>
         </Box>
-        <HStack spacing={4}>
+        <Grid templateColumns='repeat(3, 1fr)' gap={4}>
+        <ProjectCard />
           <ProjectCard />
           <ProjectCard />
           <ProjectCard />
-        </HStack>
+          <ProjectCard />
+          <ProjectCard/>
+</Grid>
       </Flex>
     </Container>
   );
