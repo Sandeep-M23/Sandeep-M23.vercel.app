c7e377a805e0b89c68886d7fa1c24a1ff49817ed
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-03-12 19:35:40 +0530

    Changed Animation

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,220 +19,10 @@ import {
 import React from "react";
 
 const glitch = keyframes`
-0%, 20%, 33%, 60%, 64%, 66%, 69%, 73%, 75%, 90%, 93%, 100% {
-  transform: skew(0deg);
-}
-21%, 81% {
-  transform: skew(30deg);
-}
-61% {
-  transform: skew(-35deg);
-}
-65% {
-  transform: skew(15deg);
-}
-70% {
-  transform: skew(65deg);
-}
-74% {
-  transform: skew(-15deg);
+to {
+  background-position: 200% center;
 }
 `;
-
-const glitchTop = keyframes`
-0% {
-  clip: rect(77px, 9999px, 6px, 0);
-}
-5% {
-  clip: rect(65px, 9999px, 70px, 0);
-}
-10% {
-  clip: rect(82px, 9999px, 82px, 0);
-}
-15% {
-  clip: rect(32px, 9999px, 35px, 0);
-}
-20% {
-  clip: rect(31px, 9999px, 49px, 0);
-}
-25% {
-  clip: rect(18px, 9999px, 46px, 0);
-}
-30% {
-  clip: rect(94px, 9999px, 64px, 0);
-}
-35% {
-  clip: rect(69px, 9999px, 31px, 0);
-}
-40% {
-  clip: rect(6px, 9999px, 72px, 0);
-}
-45% {
-  clip: rect(25px, 9999px, 35px, 0);
-}
-50% {
-  clip: rect(82px, 9999px, 26px, 0);
-}
-55% {
-  clip: rect(7px, 9999px, 92px, 0);
-}
-60% {
-  clip: rect(54px, 9999px, 30px, 0);
-}
-65% {
-  clip: rect(86px, 9999px, 90px, 0);
-}
-70% {
-  clip: rect(38px, 9999px, 65px, 0);
-}
-75% {
-  clip: rect(41px, 9999px, 66px, 0);
-}
-80% {
-  clip: rect(31px, 9999px, 37px, 0);
-}
-85% {
-  clip: rect(8px, 9999px, 12px, 0);
-}
-90% {
-  clip: rect(50px, 9999px, 69px, 0);
-}
-95% {
-  clip: rect(7px, 9999px, 42px, 0);
-}
-100% {
-  clip: rect(14px, 9999px, 57px, 0);
-}`;
-
-const glitchBottom = keyframes`
-0% {
-  clip: rect(77px, 9999px, 6px, 0);
-}
-5% {
-  clip: rect(65px, 9999px, 70px, 0);
-}
-10% {
-  clip: rect(82px, 9999px, 82px, 0);
-}
-15% {
-  clip: rect(32px, 9999px, 35px, 0);
-}
-20% {
-  clip: rect(31px, 9999px, 49px, 0);
-}
-25% {
-  clip: rect(18px, 9999px, 46px, 0);
-}
-30% {
-  clip: rect(94px, 9999px, 64px, 0);
-}
-35% {
-  clip: rect(69px, 9999px, 31px, 0);
-}
-40% {
-  clip: rect(6px, 9999px, 72px, 0);
-}
-45% {
-  clip: rect(25px, 9999px, 35px, 0);
-}
-50% {
-  clip: rect(82px, 9999px, 26px, 0);
-}
-55% {
-  clip: rect(7px, 9999px, 92px, 0);
-}
-60% {
-  clip: rect(54px, 9999px, 30px, 0);
-}
-65% {
-  clip: rect(86px, 9999px, 90px, 0);
-}
-70% {
-  clip: rect(38px, 9999px, 65px, 0);
-}
-75% {
-  clip: rect(41px, 9999px, 66px, 0);
-}
-80% {
-  clip: rect(31px, 9999px, 37px, 0);
-}
-85% {
-  clip: rect(8px, 9999px, 12px, 0);
-}
-90% {
-  clip: rect(50px, 9999px, 69px, 0);
-}
-95% {
-  clip: rect(7px, 9999px, 42px, 0);
-}
-100% {
-  clip: rect(14px, 9999px, 57px, 0);
-}
-}
-@keyframes noise-anim-2 {
-0% {
-  clip: rect(18px, 9999px, 13px, 0);
-}
-5% {
-  clip: rect(74px, 9999px, 51px, 0);
-}
-10% {
-  clip: rect(33px, 9999px, 65px, 0);
-}
-15% {
-  clip: rect(64px, 9999px, 66px, 0);
-}
-20% {
-  clip: rect(75px, 9999px, 6px, 0);
-}
-25% {
-  clip: rect(40px, 9999px, 49px, 0);
-}
-30% {
-  clip: rect(71px, 9999px, 1px, 0);
-}
-35% {
-  clip: rect(42px, 9999px, 73px, 0);
-}
-40% {
-  clip: rect(92px, 9999px, 57px, 0);
-}
-45% {
-  clip: rect(4px, 9999px, 33px, 0);
-}
-50% {
-  clip: rect(4px, 9999px, 34px, 0);
-}
-55% {
-  clip: rect(49px, 9999px, 25px, 0);
-}
-60% {
-  clip: rect(24px, 9999px, 44px, 0);
-}
-65% {
-  clip: rect(31px, 9999px, 99px, 0);
-}
-70% {
-  clip: rect(54px, 9999px, 83px, 0);
-}
-75% {
-  clip: rect(57px, 9999px, 97px, 0);
-}
-80% {
-  clip: rect(3px, 9999px, 15px, 0);
-}
-85% {
-  clip: rect(84px, 9999px, 34px, 0);
-}
-90% {
-  clip: rect(100px, 9999px, 88px, 0);
-}
-95% {
-  clip: rect(91px, 9999px, 59px, 0);
-}
-100% {
-  clip: rect(10px, 9999px, 87px, 0);
-}`;
 
 const Home = () => {
   const LinkBgDarkModeColor = "#272727";
@@ -241,15 +31,7 @@ const Home = () => {
 
   const animation = prefersReducedMotion
     ? undefined
-    : `${glitch} 5s linear infinite`;
-
-  const animationTop = prefersReducedMotion
-    ? undefined
-    : `${glitchTop} 3s linear alternate-reverse infinite`;
-
-  const animationBottom = prefersReducedMotion
-    ? undefined
-    : `${glitchBottom} 5s linear alternate-reverse infinite`;
+    : `${glitch} 6s ease-in-out reverse infinite`;
 
   return (
     <Container maxW={"6xl"} mt={24}>
@@ -258,31 +40,12 @@ const Home = () => {
           <Heading
             size={"3xl"}
             animation={animation}
-            position="relative"
-            zIndex="1"
-            left="0"
-            right="0"
-            bottom="0"
-            _before={{
-              position: "absolute",
-              content: `"I'm Sandeep M"`,
-              animation: animationTop,
-              top: 0,
-              left: 0,
-              clip: "rect(0, 900px, 0, 0)",
-              filter: "drop-shadow(2px 2px 2px rgba(35,184,197,1))",
-              opacity: 0.8,
-            }}
-            _after={{
-              position: "absolute",
-              content: `"I'm Sandeep M"`,
-              animation: animationBottom,
-              top: 0,
-              left: 0,
-              clip: "rect(0, 900px, 0, 0)",
-              filter: "drop-shadow(2px 2px 2px rgba(274,27,27,1))",
-              opacity: 0.8,
-            }}
+            textTransform=" uppercase"
+            backgroundImage="linear-gradient(245deg, #f4f1ee 0%, #FED7D7 29%,#F56565  49%,#C53030 67%,#822727 100%)"
+            backgroundSize="200% auto"
+            textFillColor="transparent"
+            backgroundClip="text"
+            display="inline-block"
           >
             I&apos;m Sandeep M
           </Heading>
