8d172d223e8b459178979c3ac8eb3b64ad2f0139
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-03-13 23:26:31 +0530

    Added Page Transistion

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,8 +2,10 @@ import { ChakraProvider } from "@chakra-ui/react";
 import NavigationBar from "../components/NavigationBar/NavigationBar";
 import Head from "next/head";
 import theme from "../styles/theme";
+import {motion,AnimatePresence} from "framer-motion";
 
-const App = ({ Component, pageProps }) => {
+const App = ({ Component, pageProps,router }) => {
+  
   return (
     <>
       <Head>
@@ -12,7 +14,33 @@ const App = ({ Component, pageProps }) => {
       </Head>
       <ChakraProvider theme={theme}>
         <NavigationBar /> {/*CHANGED LOCATION FOR GLOBAL REFERENCE*/}
-        <Component {...pageProps} />
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            initial="initial"
+            animate="animate"
+            transition="0.7"
+            exit="exit"
+            variants={{
+              initial: {
+                opacity: 0,
+                left: "-100%",
+                scale: 0.6,
+              },
+              animate: {
+                opacity: 1,
+                left: 0,
+                scale: 1,
+              },
+              exit: {
+                opacity: 0,
+                left: "100%",
+                scale: 0.6,
+              },
+            }}
+          >
+            <Component {...pageProps} key={router.route}/>
+          </motion.div>
+        </AnimatePresence>
       </ChakraProvider>
     </>
   );
