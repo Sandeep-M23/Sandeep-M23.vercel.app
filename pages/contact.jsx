cd1cda16d3dd2e19283537a95454db91207827f5
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-03-21 19:29:32 +0530

    Added Form Functionality

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -14,13 +14,75 @@ import {
   Text,
   useMediaQuery,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import { BsPerson } from "react-icons/bs";
 import { MdOutlineEmail } from "react-icons/md";
 import ImageCard from "../components/UI/Card/ImageCard";
+import SimpleModal from "../components/UI/Modal/Modal";
 
 const Contact = () => {
   const [isMobile] = useMediaQuery("(max-width: 768px)");
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const subjectRef = useRef(null);
+  const messageRef = useRef(null);
+  const [formMessage, setFormMessage] = useState({
+    title: "",
+    message: "",
+  });
+  const [formSubmitted,setFormSubmitted] = useState(false);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const templateParams = {
+      from_name: nameRef.current.value,
+      from_email: emailRef.current.value,
+      to_name: "Sandeep M",
+      message: messageRef.current.value,
+      subject: subjectRef.current.value,
+    };
+
+    emailjs
+      .send(
+        "service_6plk3re",
+        "template_1u212jk",
+        templateParams,
+        "KcwfnCu4p4AJfccFi"
+      )
+      .then(
+        ({ status }) => {
+          if (status === 200) {
+            setFormSubmitted(true);
+            setFormMessage({
+              title: "Message has been sent",
+              message: "Will be in contact with you soon.",
+            });
+            e.target.reset();
+          } else {
+            setFormSubmitted(true);
+            setFormMessage({
+              title: "Unexpected Error Occured",
+              message:
+                "Try again later! Please contact by phone or other social platforms",
+            });
+            e.target.reset();
+          }
+        },
+        (error) => {
+          // eslint-disable-next-line no-console
+          console.log(error);
+          setFormSubmitted(true)
+          setFormMessage({
+            title: "Error sending message, try again later",
+            paragraph: "Please contact by phone or other social platforms",
+          });
+          e.target.reset();
+        }
+      );
+  };
+
   return (
     <Container maxW={"6xl"} centerContent overflow="hidden">
       <Box borderRadius="lg" m={{ base: 2 }} p={{ base: 2 }} width={"90%"}>
@@ -59,25 +121,33 @@ const Contact = () => {
             Got a question or proposal, or just want to say hello? Go ahead.
           </Text>
         </Stack>
-        <Box margin={{ base: 2, lg: 6 }} mt={8} mb={0} display={"flex"}>
-          {isMobile ? <div display="none"></div> : <ImageCard />}
+        <Box
+          margin={{ base: 2, lg: 4 }}
+          mt={8}
+          mb={0}
+          display={"flex"}
+          as={"form"}
+          onSubmit={onSubmitHandler}
+        >
+          {isMobile ? null : <ImageCard />}
           <VStack
             spacing={3}
             width={"100%"}
-            p={6}
+            p={5}
             bg="white"
             color="black"
             roundedTopRight={8}
             roundedBottomRight={8}
             roundedLeft={{ base: 8, lg: 0 }}
           >
-            <FormControl id="name">
+            <FormControl id="name" isRequired>
               <FormLabel>Your Name</FormLabel>
               <InputGroup borderColor="#E0E1E7">
                 <InputLeftElement pointerEvents="none">
                   <BsPerson color="gray.800" />
                 </InputLeftElement>
                 <Input
+                  ref={nameRef}
                   type="text"
                   size="md"
                   borderColor="gray.300"
@@ -91,13 +161,14 @@ const Contact = () => {
                 />
               </InputGroup>
             </FormControl>
-            <FormControl id="email">
+            <FormControl id="email" isRequired>
               <FormLabel>Mail</FormLabel>
               <InputGroup borderColor="#E0E1E7">
                 <InputLeftElement pointerEvents="none">
                   <MdOutlineEmail color="gray.800" />
                 </InputLeftElement>
                 <Input
+                  ref={emailRef}
                   type="text"
                   size="md"
                   borderColor="gray.300"
@@ -111,9 +182,10 @@ const Contact = () => {
                 />
               </InputGroup>
             </FormControl>
-            <FormControl id="subject">
+            <FormControl id="subject" isRequired>
               <FormLabel>Subject</FormLabel>
               <Input
+                ref={subjectRef}
                 type="text"
                 size="md"
                 borderColor="gray.300"
@@ -126,9 +198,10 @@ const Contact = () => {
                 }}
               />
             </FormControl>
-            <FormControl id="message">
+            <FormControl id="message" isRequired>
               <FormLabel>Message</FormLabel>
               <Textarea
+                ref={messageRef}
                 borderColor="gray.300"
                 _hover={{
                   borderRadius: "gray.300",
@@ -141,9 +214,11 @@ const Contact = () => {
             </FormControl>
             <FormControl id="button" float="right">
               <Button
+                type="submit"
                 variant="solid"
                 bg="red.500"
                 color="white"
+                _focus={{ outline: "none" }}
                 _hover={{
                   bg: "red.700",
                 }}
@@ -154,6 +229,14 @@ const Contact = () => {
           </VStack>
         </Box>
       </Box>
+      {formSubmitted && (
+        <SimpleModal
+          isOpen={formSubmitted}
+          message={formMessage.message}
+          title={formMessage.title}
+          onClose={() => setFormSubmitted(false)}
+        />
+      )}
     </Container>
   );
 };
