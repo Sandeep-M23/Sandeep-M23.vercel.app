92cf814327e41092f1002f5d6fdf7c5650f6c2cc
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-03-23 19:43:52 +0530

    Removed Animation

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import NavigationBar from "../components/NavigationBar/NavigationBar";
 import Head from "next/head";
 import theme from "../styles/theme";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const App = ({ Component, pageProps, router }) => {
   return (
@@ -12,33 +12,9 @@ const App = ({ Component, pageProps, router }) => {
         <meta name="description" content="PortFolio Website of Sandeep M" />
       </Head>
       <ChakraProvider theme={theme}>
-        <NavigationBar /> {/*CHANGED LOCATION FOR GLOBAL REFERENCE*/}
         <AnimatePresence exitBeforeEnter>
-          <motion.div
-            initial="initial"
-            animate="animate"
-            transition="0.7"
-            exit="exit"
-            variants={{
-              initial: {
-                opacity: 0,
-                left: "-100%",
-                scale: 0.6,
-              },
-              animate: {
-                opacity: 1,
-                left: 0,
-                scale: 1,
-              },
-              exit: {
-                opacity: 0,
-                left: "100%",
-                scale: 0.6,
-              },
-            }}
-          >
-            <Component {...pageProps} key={router.route} />
-          </motion.div>
+          <NavigationBar /> {/*CHANGED LOCATION FOR GLOBAL REFERENCE*/}
+          <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </ChakraProvider>
     </>
