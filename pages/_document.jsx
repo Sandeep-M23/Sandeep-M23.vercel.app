703f42dbb500ed49d96f06f8fb8af7c138bb53be
sandeep <sandeep.m24.rathnam@gmail.com>
2025-05-19 21:27:04 +0530

    Minor change

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -12,7 +12,7 @@ export default function Document() {
             src='https://www.googletagmanager.com/ns.html?id=GTM-WF7K3Q6W'
             height='0'
             width='0'
-            style='display:none;visibility:hidden'
+            style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
         </noscript>
         <Main />
